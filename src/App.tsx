335c4227df10570cdb4bf3c63b8622361629e700
IgorYudintsev <kakvospitat@gmail.com>
2022-03-24 09:20:27 +0300

    classic

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,27 @@
-import React, {useState} from 'react';
+import React from 'react';
+import logo from './logo.svg';
 import './App.css';
-import Header from "./components/Header";
-import {Footer} from "./components/Footer";
-import Content from "./components/Content";
-import {Navigation} from "./components/Navigation";
 
 function App() {
-    let [cargoOfskills, setCargoOfskills] = useState([
-        {id: 1, skill: "HTML&CSS", isDone: true},
-        {id: 2, skill: "Flex", isDone: true},
-        {id: 3, skill: "GRID", isDone: false},
-        {id: 4, skill: "StyledComponents", isDone: true},
-        {id: 5, skill: "JS", isDone: true},
-    ])
-
-    const arrForFooterHeder = ['HEADER', 'FOOTER']
-    return (
-        <div>
-            <Header name={'header'}/>
-            <div className={'Body'}>
-                <Navigation/>
-                <Content skills1={cargoOfskills} cargoOfskills={cargoOfskills} setCargoOfskills={setCargoOfskills}/>
-            </div>
-            <Footer name={arrForFooterHeder}/>
-        </div>
-
-    );
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Hellow World</h1>
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
--- src/components/Page3.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export const Page3 = () => {
-    return (
-        <h1>PAGE3</h1>
-    );
-};
